b9294e3ff7fa8f0b7c250aabb574c039867cf106
jkhoang313 <jkhoang313@gmail.com>
2017-01-18 16:47:28 -0500

    Done

--- js/card.js
+++ js/card.js
@@ -2,10 +2,21 @@ $( function(){
 
   $( "#card-one").click(function(){
     //Use toggleClass method here
+    $('#card-one').toggleClass("flipped")
   });
 
 
 	// code click event for card-two here
 
+  $( "#card-two").click(function(){
+    //Use toggleClass method here
+    $('#card-two').toggleClass("flipped")
+  });
+
   // code click event for card-three here
+
+  $( "#card-three").click(function(){
+    //Use toggleClass method here
+    $('#card-three').toggleClass("flipped")
+  });
  })
